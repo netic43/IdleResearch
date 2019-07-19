087dbf068b91fc8868e44c1634dbdaae093ca1ec
Nitrodev <Nitroglyserin1@gmail.com>
2019-07-19 14:29:48 +0300

    Init

---
+++ main.js
@@ -0,0 +1,38 @@
+var player = {
+    knowledge: 0,
+    totalRate: 0
+}
+
+function study() {
+    player.knowledge++;
+    $("#knowledge").html(player.knowledge);
+}
+
+function purchase(id) {
+    for(res of workers) {
+        if(res.id == id)
+            if(player.knowledge >= res.cost) {
+                player.knowledge-=res.cost;
+                res.num++;
+                res.cost = Math.ceil(res.cost * 1.75);
+                player.totalRate += res.rate;
+                updateRes(res);
+                $("#knowledge").html(Math.floor(player.knowledge));
+                $("#totalRate").html(player.totalRate + " knowledge per second");
+            }
+    }
+}
+
+function updateRes(obj) {
+    $("#resNum"+obj.id).html(obj.num);
+    $("#resCost"+obj.id).html(obj.cost);
+}
+
+function autoKnowledge() {
+    player.knowledge += (player.totalRate/10);
+    $("#knowledge").html(Math.floor(player.knowledge));
+}
+
+$(function() {
+    setInterval(autoKnowledge,100);
+});
---
+++ researchers.js
@@ -0,0 +1,38 @@
+var workers = [
+    intern = {
+        id: 0,
+        num: 0,
+        cost: 10,
+        rate: 1
+    },
+    highschool = {
+        id: 1,
+        num: 0,
+        cost: 50,
+        rate: 5
+    },
+    college = {
+        id: 2,
+        num: 0,
+        cost: 500,
+        rate: 10
+    },
+    jr_assistant = {
+        id: 3,
+        num: 0,
+        cost: 1000,
+        rate: 30
+    },
+    assistant = {
+        id: 4,
+        num: 0,
+        cost: 5000,
+        rate: 70
+    },
+    senior_assistant = {
+        id: 5,
+        num: 0,
+        cost: 10000,
+        rate: 150
+    }
+];
