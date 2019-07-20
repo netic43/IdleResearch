12835942209c84a9b0fd1ed54676147d15427252
Nitrodev <Nitroglyserin1@gmail.com>
2019-07-20 15:13:30 +0300

    Saving added

--- main.js
+++ main.js
@@ -32,6 +32,34 @@ function autoKnowledge() {
     $("#knowledge").html(Math.floor(player.knowledge));
 }
 
+function saveTheGame() {
+    var save = {
+        knowledge: player.knowledge,
+        totalRate: player.totalRate,
+        workers: workers
+    }
+    localStorage.setItem("save", JSON.stringify(save));
+    //console.log("game saved");
+}
+
+function load() {
+    var savegame = JSON.parse(localStorage.getItem("save"));
+
+    if(typeof savegame !== "undefined") player.knowledge = savegame.knowledge;
+    if(typeof savegame !== "undefined") player.totalRate = savegame.totalRate;
+    if(typeof savegame !== "undefined") workers = savegame.workers;
+
+    for(res of workers) { 
+        updateRes(res);
+    }
+    $("#totalRate").html(player.totalRate + " knowledge per second");
+}
+
 $(function() {
+    if(localStorage.getItem("save")) {
+        //console.log("there is a savegame")
+        load();
+    }
+
     setInterval(autoKnowledge,100);
 });
