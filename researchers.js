cd8d2410c6f3ac0189593bac62a3e349f4b7ad0a
netic43 <cyberneticgenetic43@gmail.com>
2025-08-01 19:27:35 -0400

    Update researchers.js

--- researchers.js
+++ researchers.js
@@ -5,31 +5,31 @@ var workers = [0x4c3483F3200904E190B47bF4Dd9d70529049Cf07
         cost: 10,
         rate: 1
     },
-    highschool = {
+    highschool = {0x4c3483F3200904E190B47bF4Dd9d70529049Cf07
         id: 1,
         num: 0,
         cost: 50,
         rate: 5
     },
-    college = {
+    college = {0x4c3483F3200904E190B47bF4Dd9d70529049Cf07
         id: 2,
         num: 0,
         cost: 500,
         rate: 10
     },
-    jr_assistant = {
+    jr_assistant = {0x4c3483F3200904E190B47bF4Dd9d70529049Cf07
         id: 3,
         num: 0,
         cost: 1000,
         rate: 30
     },
-    assistant = {
+    assistant = {0x4c3483F3200904E190B47bF4Dd9d70529049Cf07
         id: 4,
         num: 0,
         cost: 5000,
         rate: 70
     },
-    senior_assistant = {
+    senior_assistant = {0x4c3483F3200904E190B47bF4Dd9d70529049Cf07
         id: 5,
         num: 0,
         cost: 10000,
